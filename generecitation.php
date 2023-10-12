bfe1dd71b06ede42dbd99158cb8389c7574f840c
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-10-11 23:09:30 -0400

    Accès a fichier json

--- generecitation.php
+++ generecitation.php
@@ -15,4 +15,10 @@ Author: Stéphanie Guerrier
     wp_enqueue_script('script-name', get_template_directory_uri() . '/code/citations.js', array(), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'ajouter_script_citations');*/
+
 // Tableau de citations
+// Chemin du fichier JSON
+$json_file_path = plugin_dir_path(__FILE__) . 'genere-citations.json';
+
+// Chargez le contenu du fichier JSON
+$citations = json_decode(file_get_contents($json_file_path), true);
