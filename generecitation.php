8101751473fd9ccb661abd96a542fca6555cee62
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-10-11 23:05:54 -0400

    Ajout de fichier JSON qui contient les citations et auteurs

--- generecitation.php
+++ generecitation.php
@@ -8,10 +8,11 @@ Author: Stéphanie Guerrier
 
 
 //fonction citation aleatoire
-function ajouter_script_citations()
+/*function ajouter_script_citations()
 {
     // wp_enqueue_script('citations-script', get_template_directory_uri() . './code/citations.js', array('javascript'), '1.0', true);
     // wp_enqueue_script('citations-script', get_template_directory_uri() . './data/genere-citations.json', array('json'), '1.0', true);
     wp_enqueue_script('script-name', get_template_directory_uri() . '/code/citations.js', array(), '1.0.0', true);
 }
-add_action('wp_enqueue_scripts', 'ajouter_script_citations');
+add_action('wp_enqueue_scripts', 'ajouter_script_citations');*/
+// Tableau de citations
