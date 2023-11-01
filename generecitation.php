970fda724153fc1784d62644db81c5b8daf85ba9
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-11-01 06:27:58 -0400

    changement code pour générer citations

--- generecitation.php
+++ generecitation.php
@@ -5,30 +5,44 @@ Description: Génère des citations aléatoires après un certain délai.
 Version: 1.0
 Author: DiversiTIM
 */
-
-
-//fonction citation aleatoire
-/*function ajouter_script_citations()
-{
-    // wp_enqueue_script('citations-script', get_template_directory_uri() . './code/citations.js', array('javascript'), '1.0', true);
-    // wp_enqueue_script('citations-script', get_template_directory_uri() . './data/genere-citations.json', array('json'), '1.0', true);
-    wp_enqueue_script('script-name', get_template_directory_uri() . '/code/citations.js', array(), '1.0.0', true);
-}
-add_action('wp_enqueue_scripts', 'ajouter_script_citations');*/
-
-// Tableau de citations
-// Chemin du fichier JSON
-$json_file_path = plugin_dir_path(__FILE__) . 'genere-citations.json';
-
-// Chargez le contenu du fichier JSON
-$citations = json_decode(file_get_contents($json_file_path), true);
-
 // Fonction pour obtenir une citation aléatoire
 function obtenir_citation()
 {
-    global $citations;
+    // Tableau de citations
+    // Chemin du fichier JSON
+    $json_file_path = plugin_dir_path(__FILE__) . 'genere-citations.json';
+
+    // Chargez le contenu du fichier JSON
+    $citations = json_decode(file_get_contents($json_file_path), true);
+
     $citation = $citations[array_rand($citations)];
     return $citation['citation'] . " - " . $citation['auteur'];
 }
 
-add_shortcode('commentaires', 'obtenir_citation');
+// Shortcode pour afficher la citation et actualiser en utilisant JavaScript
+function citation_aleatoire()
+{
+    // Générez la citation aléatoire
+    $random_citation = obtenir_citation();
+
+    // Affichez la citation
+    echo '<div id="random-citation">' . $random_citation . '</div>';
+
+    // Ajoutez du JavaScript pour actualiser la citation après un délai
+    echo '<script>
+        function actualiserCitation() {
+            var randomCitationElement = document.getElementById("random-citation");
+
+            // Générez la nouvelle citation à chaque actualisation
+            var nouvelleCitation = "' . obtenir_citation() . '";
+
+            // Mettez à jour le contenu de #random-citation
+            randomCitationElement.innerHTML =" ' . obtenir_citation() . '";
+        }
+
+        // Actualisez la citation toutes les 4 secondes (4000 millisecondes)
+        setInterval(actualiserCitation, 4000);
+    </script>';
+}
+
+add_shortcode('citations', 'citation_aleatoire');
