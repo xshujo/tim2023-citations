01a59851585ea788b9cdef6080a87572d6eb6f3f
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-11-10 10:44:43 -0500

    tentative de débuggage pour afficher le plugin dans la page d'accueil

--- citations.js
+++ citations.js
@@ -1,9 +1,14 @@
 // Fonction pour charger et afficher une nouvelle citation
 function chargerNouvelleCitation() {
   // Chargement des citations depuis le fichier JSON dans le dossier "data"
-  fetch("data/genere-citations.json")
-    .then((response) => response.json())
+  fetch(`./data/genere-citations.json`)
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
     .then((data) => {
+      console.log(data);
+
       // Sélection aléatoire d'une citation
       const randomIndex = Math.floor(Math.random() * data.length);
       const citation = data[randomIndex].citation;
@@ -12,6 +17,10 @@ function chargerNouvelleCitation() {
       // Affichage de la citation dans le HTML
       document.getElementById("citation").textContent = citation;
       document.getElementById("auteur").textContent = `- ${auteur}`;
+    })
+    .catch((err) => {
+      // Fait quelque chose por une erreur ici
+      console.log("Error Reading data " + err);
     });
 }
 
--- generateur-citations.php
+++ generateur-citations.php
@@ -5,31 +5,30 @@ Description: Génère des citations aléatoires après un certain délai.
 Version: 1.0
 Author: DiversiTIM
 */
+
 function enfiler_script_css()
 {
-    $version_css =  filemtime(plugin_dir_path(__FILE__) . 'style.css');
-    $version_js = filemtime(plugin_dir_path(__FILE__) . 'citations.js');
+    // $version_css =  filemtime(plugin_dir_path(__FILE__) . 'style.css');
+    //$version_js = filemtime(plugin_dir_path(__FILE__) . 'citations.js');
     wp_enqueue_style(
         'style_generateur_citations',
         plugin_dir_url(__FILE__) . 'style.css',
         array(),
-        $version_css
+        true
     );
     wp_enqueue_script(
         'js_citations',
         plugin_dir_url(__FILE__) . 'citations.js',
-        array(),
-        $version_js,
-        true
+        array()
     );
 }
 add_action('wp_enqueue_scripts', 'enfiler_script_css');
 function affiche_citation()
 {
-    return '<section>
+    return '<div class="genCite">
         <blockquote id="citation"></blockquote>
         <p id="auteur"></p>
-      </section>';
+      </div>';
 }
 
 add_shortcode('citations', 'affiche_citation');
