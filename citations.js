5fbc3319ba90140a104fc21a527c798db9abae39
Taïsha DM <xshujo@gmail.com>
2023-11-22 13:17:51 -0500

    Correction des fautes de grammaire des citations

--- citations.js
+++ citations.js
@@ -2,16 +2,16 @@
 const citationsData = [
   {
     citation:
-      "institution chaleureuse,  les Québécois sont tellement spéciaux!  accueillants,  souriants, que des gens heureux,  des profs dévoués, vers un art nouveau, si je reprends Pauline julien. ",
+      "Institution chaleureuse, les Québécois sont tellement spéciaux! Accueillants, souriants, que des gens heureux, des profs dévoués, vers un art nouveau, si je reprends Pauline Julien.",
     auteur: "Johanne Loken (Ancienne Timienne 2019)",
   },
   {
-    citation: "La meilleur technique du collège Maisonneuve, le TIM582.A1",
+    citation: "La meilleure technique du Collège Maisonneuve, le TIM582.A1",
     auteur: "Jonathan Grenier (Ancien Timien 2020)",
   },
   {
     citation:
-      "Excellent travail fait dans ce département pour la motivation et la dynamique des enseignants et des jeunes qui auront à la fin , un très bon travail grâce à ce concept que nous je félicite Bravo.",
+      "Excellent travail fait dans ce département pour la motivation et la dynamique des enseignants et des jeunes qui auront à la fin, un très bon travail grâce à ce concept que nous je félicite. Bravo.",
     auteur: "Louise Soucy",
   },
   {
@@ -21,17 +21,17 @@ const citationsData = [
   },
   {
     citation:
-      "Bravo! à ces formations, qui conduisent à de très bons emplois, ce qui prouve que l'instruction, la formation, l'apprentissage et les stages en industries, sont tributaires d'une sécurité d'emplois et de nouveaux défis.",
-    auteur: "Jean Longpre ",
+      "Bravo! À ces formations, qui conduisent à de très bons emplois, ce qui prouve que l'instruction, la formation, l'apprentissage et les stages en industries, sont tributaires d'une sécurité d'emplois et de nouveaux défis.",
+    auteur: "Jean Longpre",
   },
   {
     citation:
-      "Vous avez entre Vos Mains la Créativité! exprimez Vous, allez au profond de celle-ci en Nous retransmettant l'émotion du moment!  et c'est Vous qui en sortira grandit a chaque expérience! Vibré l'instant Présent...",
-    auteur: "RS Lafleur ",
+      "Vous avez entre vos mains la créativité! Exprimez-vous, allez au fond de celle-ci en nous retransmettant l'émotion du moment! Et c'est vous qui en sortirez grandi à chaque expérience! Vibrez l'instant présent...",
+    auteur: "RS Lafleur",
   },
   {
     citation:
-      "Le mouvement et la dinamique du groupe ne doit jamais fléchir et participer à tout moment à  la bonne marche de l'équipe et nous autres qui les suivons, tous devons si on le peut les soutenir dans leurs projets présents et futurs bonne chance à tous ",
+      "Le mouvement et la dynamique du groupe ne doivent jamais fléchir et participer à tout moment à la bonne marche de l'équipe et nous autres qui les suivons, tous devons, si on le peut, les soutenir dans leurs projets présents et futurs. Bonne chance à tous.",
     auteur: "Gabriel Crisante",
   },
   {
@@ -46,7 +46,7 @@ const citationsData = [
   },
   {
     citation:
-      "Être au TIM c'est une expérience qui va au-delà des écrans, transformant chaque projet en une aventure immersive et enrichissante.",
+      "Être au TIM, c'est une expérience qui va au-delà des écrans, transformant chaque projet en une aventure immersive et enrichissante.",
     auteur: "Kajanthan Seevarathinam (Finissant TIM 2024)",
   },
 ];
